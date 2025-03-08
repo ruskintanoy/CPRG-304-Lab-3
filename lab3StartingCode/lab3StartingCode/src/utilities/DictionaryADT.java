7c21996dcce1e109e0da061e4d9108066aa0c5c3
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-03-07 22:20:26 -0700

    Add files via upload

---
+++ lab3StartingCode/lab3StartingCode/src/utilities/DictionaryADT.java
@@ -0,0 +1,16 @@
+package utilities;
+
+/**
+* DictionaryADT.java
+*
+* @author your name
+* @version major.minor revision number starting at 1.0
+* 
+* Class Definition: This interface represents the public contract for the
+* implementation of Dictionary for the DictionaryADT Lab. 
+*/
+
+public interface DictionaryADT<K,V>
+{
+
+}
