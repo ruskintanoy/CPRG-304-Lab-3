3a7c8f932859495d8025f488b40b39cca1a36a19
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-03-07 22:30:55 -0700

    Initial commit of starting code

---
+++ lab3StartingCode/lab3StartingCode/src/implementations/Dictionary.java
@@ -0,0 +1,16 @@
+package implementations;
+
+import java.util.ArrayList;
+
+import utilities.DictionaryADT;
+
+public class Dictionary<K,V> implements DictionaryADT<K,V>
+{
+	// constant
+	private static final int DEFAULT_SIZE = 10;
+	
+	// index indicates pairing, e.g. keys[1] is stored at values[1]
+	private ArrayList<K> keys;
+	private ArrayList<V> values;
+
+}
